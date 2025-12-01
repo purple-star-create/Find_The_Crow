eb2687533c1250fd103d92a9640bb60b0dacd139
purple-star-create <219644528+purple-star-create@users.noreply.github.com>
2025-12-01 17:11:13 +0100

    Link change test

--- docs/js/script_site_game_page_1.js
+++ docs/js/script_site_game_page_1.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		console.log("crow was clicked")
 
 		if($('img.crow').hasClass('etat3') ){
-			window.location.href = 'file:///C:/Users/Alice/Desktop/FIND_THE_CROW/FICHIERS/docs/site_game_page_2.html';
+			window.location.href = 'https://purple-star-create.github.io/Find_The_Crow/site_game_page_2.html';
 		}
 
 		if($('img.crow').hasClass('etat2') ){
